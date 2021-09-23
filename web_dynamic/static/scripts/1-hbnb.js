aba1c26cbf0a5649e1f97a301f2950a7106d28be
RaimundoGallino <raigallino0@gmail.com>
2021-09-23 16:40:49 -0300

    asd

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,7 @@
 window.onload = function() {
-    console.log("a");
     let listAmenities = $("DIV.amenities DIV.popover ul li");
-    console.log(listAmenities);
-    console.log(listAmenities.text());
     for (let li of listAmenities) {
-        console.log("CCCCCCC");
-        console.log(li);
-        console.log("CCCCCC");
-        console.log($(li).first().val());
         $(li).first().click(function() {
-            console.log("d");
-            return;
             let listAmenities = $("DIV.amenities DIV.popover ul li");
             let selectedAmenitiesText = "";
             $('DIV.amenities').first().next().empty();
