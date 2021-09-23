d58212446503c2a4edd5230bb45a3aa62780d485
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-23 18:34:38 -0300

    andas?

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,34 +1,26 @@
 $(document).ready(function () {
   let listAmenities = $("DIV.amenities DIV.popover ul li");
+  let checkedAmenities = [];
 
   for (let li of listAmenities) {
-    console.log($(li).children(":first"));
-    console.log("========================");
-    $(li).children(":first").change(function () {
-      let checkedAmenities = [];
-      let dataName = $(li).text();
-      console.log(dataName);
-      console.log($(li).first().text());
-      console.log($(this));
-      console.log(this);
-      console.log("I'm Checked? " + $(li).first().is(':checked'));
-      console.log("I'm Checked? " + $(li).first().attr('checked'));
-      console.log("I'm Checked? " + $(li).first().prop('checked'));
-      console.log("with this: " + this.checked);
-      if (this.checked) {
-        checkedAmenities.push(dataName);
-      } else {
-        let idx = checkedAmenities.indexOf(dataName);
-        if (idx !== -1) {
-          checkedAmenities.splice(idx, 1);
-        }
-      }
-
-      if (checkedAmenities.length > 0) {
-        $('div.amenities h4').text(checkedAmenities.join(', '));
-      } else {
-        $('div.amenities h4').text('&nbsp;');
-      }
-    });
+    $(li).children(":first").change(changeDelegate(checkedAmenities));
   }
 });
+
+let changeDelegate = function(checkedList) {
+  let dataName = $(li).text();
+  if (this.checked) {
+    checkedList.push(dataName);
+  } else {
+    let idx = checkedList.indexOf(dataName);
+    if (idx !== -1) {
+      checkedList.splice(idx, 1);
+    }
+  }
+
+  if (checkedList.length > 0) {
+    $('div.amenities h4').text(checkedList.join(', '));
+  } else {
+    $('div.amenities h4').html('&nbsp;');
+  }
+};
