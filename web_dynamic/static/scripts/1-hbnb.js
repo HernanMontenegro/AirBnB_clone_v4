aba491948a4c32adcbd06ce4640a68054c37bfac
RaimundoGallino <raigallino0@gmail.com>
2021-09-23 16:10:51 -0300

    Co-authored-by: Hernan Montenegro <HernanMontenegro@users.noreply.github.com>

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,15 @@
 window.onload = function() {
+    console.log("a");
     let listAmenities = $("DIV.amenities.popover ul li");
     for (let li of listAmenities) {
+        console.log(`b ${li}`);
         $(li).first().change(fillAmenities());
     }
 };
 
 function fillAmenities()
 {
+    console.log("c");
     let listAmenities = $("DIV.amenities.popover ul li");
     let selectedAmenitiesText = "";
     $('DIV.amenities h4').empty();
