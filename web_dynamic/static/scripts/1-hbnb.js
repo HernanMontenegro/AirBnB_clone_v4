b1bf3cb5ea39b6d8b04f6fe5d21825ccb69a93e2
RaimundoGallino <raigallino0@gmail.com>
2021-09-23 17:36:12 -0300

    a

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,29 +1,24 @@
-window.onload = function() {
-    let listAmenities = $("DIV.amenities DIV.popover ul li");
-    let checkedOnes = [];
+window.onload = function () {
+  let listAmenities = $("DIV.amenities DIV.popover ul li");
+  let checkedOnes = [];
 
-    for (let li of listAmenities) {
-        $(li).first().click(function() {
-            let amenCheckState = $(li).first();
-            let amenName = $(li).text();
-            let h4 = $('DIV.amenities h4');
+  for (let li of listAmenities) {
+    $(li).first().click(function () {
+      let checkedAmenities = [];
+      let dataName = $(this).data('name');
+      if (this.checked) {
+        checkedAmenities.push(dataName);
+      } else {
+        let idx = checkedAmenities.indexOf(dataName);
+        if (idx !== -1) {
+          checkedAmenities.splice(idx, 1);
+        }
+      }
 
-            console.log(amenCheckState.prop('checked'));
-
-            if (this.checked) {
-                //amenCheckState.prop('checked', false);
-                // console.log("falso");
-                let idx = array.indexOf(amenName);
-                if (idx !== -1) {
-                    array.splice(idx, 1);
-                }
-            } else {
-                //amenCheckState.prop('checked', true);
-                // console.log("true");
-                checkedOnes.push(amenName);
-            }
-
-            h4.text(checkedOnes.join(', '));
-        });
+      if (checkedAmenitiesength > 0) {
+        $('div.amenities h4').text(checkedAmenitiesength.join(', '));
+      } else {
+        $('div.amenities h4').text('&nbsp;');
+      }
     }
 };
