1c527c808e0560c365ad50f9ab7b5ed9bba54405
RaimundoGallino <raigallino0@gmail.com>
2021-09-23 17:06:03 -0300

    Co-authored-by: Hernan Montenegro <HernanMontenegro@users.noreply.github.com>

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,48 +1,17 @@
 window.onload = function() {
     let listAmenities = $("DIV.amenities DIV.popover ul li");
     for (let li of listAmenities) {
-        $(li).first().change(function() {
-            let listAmenities = $("DIV.amenities DIV.popover ul li");
-            let selectedAmenitiesText = "";
+        $(li).first().click(function() {
+            let amenName = $(li).first().next().text();
             let h4 = $('DIV.amenities h4');
-            let firstTime = true;
-
-            console.log(h4);
-            console.log(h4.text());
-
-            h4.empty();
-
-            for (let li of listAmenities) {
-                let amenCheckState = $(li).first();
-                let amenName = $(li).first().next().text();
-                console.log("Target LI: ");
-                console.log($(li));
-                console.log("===============");
-                console.log("first child: ");
-                console.log(amenCheckState);
-                console.log("Is checked?");
-                console.log(amenCheckState.prop('checked'));
-                console.log("=============");
-                console.log("second child: ");
-                console.log(amenName);
-                console.log("=============");
-                if (amenCheckState.prop('checked') == true) {
-                    if (firstTime) {
-                        selectedAmenitiesText = amenName;
-                        firstTime = false;
-                        continue;
-                    }
-                    selectedAmenitiestext += `, ${amenName}`;
-                }
+            if (amenCheckState.is(':checked')) {
+                amenCheckState.prop('checked', false);
+                console.log("falso");
+            } else {
+                amenCheckState.prop('checked', true);
+                console.log("true");
+                h4.text(amenName);
             }
-
-            console.log("result " + selectedAmenitiesText);
-            h4.text(selectedAmenitiestext);
         });
     }
 };
-
-function fillAmenities(elem)
-{
-    
-}
