e215f5a076ae12627380e9318082ef897eb455eb
RaimundoGallino <raigallino0@gmail.com>
2021-09-23 17:17:15 -0300

    Co-authored-by: Hernan Montenegro <HernanMontenegro@users.noreply.github.com>

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,9 +1,11 @@
 window.onload = function() {
     let listAmenities = $("DIV.amenities DIV.popover ul li");
+    let checkedOnes = [];
+
     for (let li of listAmenities) {
         $(li).first().click(function() {
             let amenCheckState = $(li).first();
-            let amenName = $(li).first().next().text();
+            let amenName = $(li).text();
             let h4 = $('DIV.amenities h4');
             if (amenCheckState.is(':checked')) {
                 amenCheckState.prop('checked', false);
