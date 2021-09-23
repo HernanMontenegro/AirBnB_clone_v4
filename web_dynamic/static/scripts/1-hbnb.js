e973b4c8c11dc1784804277d409334e54252be06
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-23 18:36:19 -0300

    we

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,25 +2,25 @@ $(document).ready(function () {
   let listAmenities = $("DIV.amenities DIV.popover ul li");
   let checkedAmenities = [];
 
+  let changeDelegate = function(checkedList) {
+    let dataName = $(li).text();
+    if (this.checked) {
+      checkedList.push(dataName);
+    } else {
+      let idx = checkedList.indexOf(dataName);
+      if (idx !== -1) {
+        checkedList.splice(idx, 1);
+      }
+    }
+  
+    if (checkedList.length > 0) {
+      $('div.amenities h4').text(checkedList.join(', '));
+    } else {
+      $('div.amenities h4').html('&nbsp;');
+    }
+  };
+
   for (let li of listAmenities) {
     $(li).children(":first").change(changeDelegate(checkedAmenities));
   }
 });
-
-let changeDelegate = function(checkedList) {
-  let dataName = $(li).text();
-  if (this.checked) {
-    checkedList.push(dataName);
-  } else {
-    let idx = checkedList.indexOf(dataName);
-    if (idx !== -1) {
-      checkedList.splice(idx, 1);
-    }
-  }
-
-  if (checkedList.length > 0) {
-    $('div.amenities h4').text(checkedList.join(', '));
-  } else {
-    $('div.amenities h4').html('&nbsp;');
-  }
-};
