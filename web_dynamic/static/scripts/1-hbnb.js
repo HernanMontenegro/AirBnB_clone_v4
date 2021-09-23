99c7706372cd7f6ab94a23fe7ade623d8a32c23c
RaimundoGallino <raigallino0@gmail.com>
2021-09-23 16:34:26 -0300

    sas

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -7,26 +7,28 @@ window.onload = function() {
         console.log("CCCCCCC");
         console.log(li);
         console.log("CCCCCC");
-        $(li).first().click(fillAmenities(this));
+        console.log($(li).first());
+        $(li).first().click(function() {
+            console.log("d");
+            return;
+            let listAmenities = $("DIV.amenities DIV.popover ul li");
+            let selectedAmenitiesText = "";
+            $('DIV.amenities').first().next().empty();
+
+            for (let li of listAmenities) {
+                let amenCheckState = $(li).first();
+                let amenName = $(li).first().next().text();
+                if (amenCheckState.is(':checked')) {
+                    selectedAmenitiestext += `, ${amenName}`;
+                }
+            }
+
+            $('DIV.amenities').first().next().text(selectedAmenitiestext);
+        });
     }
 };
 
 function fillAmenities(elem)
 {
-    console.log("d");
-    console.log(elem);
-    return;
-    let listAmenities = $("DIV.amenities DIV.popover ul li");
-    let selectedAmenitiesText = "";
-    $('DIV.amenities').first().next().empty();
-
-    for (let li of listAmenities) {
-        let amenCheckState = $(li).first();
-        let amenName = $(li).first().next().text();
-        if (amenCheckState.is(':checked')) {
-            selectedAmenitiestext += `, ${amenName}`;
-        }
-    }
-
-    $('DIV.amenities').first().next().text(selectedAmenitiestext);
+    
 }
