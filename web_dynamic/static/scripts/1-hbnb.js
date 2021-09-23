cab1b4537c3c9a9de8b8544815108b9d832ca934
RaimundoGallino <raigallino0@gmail.com>
2021-09-23 17:15:13 -0300

    Co-authored-by: Hernan Montenegro <HernanMontenegro@users.noreply.github.com>

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,24 +2,16 @@ window.onload = function() {
     let listAmenities = $("DIV.amenities DIV.popover ul li");
     for (let li of listAmenities) {
         $(li).first().click(function() {
-            // let amenCheckState = $(li).first();
-            // let amenName = $(li).first().next().text();
-            // let h4 = $('DIV.amenities h4');
-            // if (amenCheckState.is(':checked')) {
-            //     amenCheckState.prop('checked', false);
-            //     console.log("falso");
-            // } else {
-            //     amenCheckState.prop('checked', true);
-            //     console.log("true");
-            //     h4.text(amenName);
-            // }
+            let amenCheckState = $(li).first();
             let amenName = $(li).first().next().text();
             let h4 = $('DIV.amenities h4');
-            if (this.checked) {
+            if (amenCheckState.is(':checked')) {
+                amenCheckState.prop('checked', false);
+                console.log("falso");
+            } else {
+                amenCheckState.prop('checked', true);
                 console.log("true");
                 h4.text(amenName);
-            } else {
-                console.log("falso");
             }
         });
     }
