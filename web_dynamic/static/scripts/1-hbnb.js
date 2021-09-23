710a04cb299fbc89238512bbce7d85f2b86ca872
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-23 18:21:33 -0300

    a

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -6,6 +6,9 @@ $(document).ready(function () {
       let checkedAmenities = [];
       let dataName = $(this).data('name');
       console.log(dataName);
+      console.log("I'm Checked? " + $(li).first());
+      console.log("I'm Checked? " + $(li).first().is(':checked'));
+      console.log("I'm Checked? " + $(li).first().attr('checked'));
       console.log("I'm Checked? " + $(li).first().prop('checked'));
       if (this.checked) {
         checkedAmenities.push(dataName);
