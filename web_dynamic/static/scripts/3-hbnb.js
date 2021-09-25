2dc05792168369ccb25bca13270434471d19bfe9
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-25 11:47:54 -0300

    e

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -33,13 +33,7 @@ $(document).ready(function () {
       $("div#api_status").removeClass("available");
       return resp.text().then(text => {throw new Error(text)}) 
     });
-
-    let place = null;
-    let user = null;
-
-    AjaxCall();
     
-
     // $.post("http://afa6415d533b.0a98cdc3.hbtn-cod.io:5001/api/v1/places_search/", {},
     // function(data, status){
     //   if (status.)
@@ -47,40 +41,53 @@ $(document).ready(function () {
     //   console.log(data);
     // });
   
-  // $.ajax({
-  //   type: 'post',
-  //   url: 'http://afa6415d533b.0a98cdc3.hbtn-cod.io:5001/api/v1/places_search/',   
-  //   contentType: 'application/json',
-  //   data: JSON.stringify({}),
-  //   xhrFields: {
-  //     withCredentials: false
-  //   },  
-  //   headers: {
+  $.ajax({
+    type: 'post',
+    url: 'http://afa6415d533b.0a98cdc3.hbtn-cod.io:5001/api/v1/places_search/',   
+    contentType: 'application/json',
+    data: JSON.stringify({}),
+    xhrFields: {
+      withCredentials: false
+    },  
+    headers: {
 
-  //   },
-  //   success: function (data) {
-  //     // data.forEach(async (element) => {
-  //     //   $("section.places").append();
+    },
+    success: function (data) {
+      console.log('Success');
+      console.log(data);
+      // Loop section
+      data.forEach(element => {
+        console.log(element);
+        let str = `<article><div class="title_box"><h2>${element.name}</h2><div class="price_by_night">$${element.price_by_night}</div></div>`;
+        str += '<div class="information">';
+          str += appendText("Guest", element.max_guest);
+          str += appendText("Bedroom", element.number_rooms);
+          str += appendText("Bathroom", element.number_bathrooms);
+        str += "</div>";
+        $("section.places").append();
 
         
-  //       /*
-  //       <div class="user">
-  //       <b>Owner:</b> ${element.user.first_name} ${element.user.last_name}</div><div class="description"> ${element.description}</div>
+        /*
+        <div class="user">
+        <b>Owner:</b> ${element.user.first_name} ${ element.user.last_name}
+      </div>
+      <div class="description">
+      ${element.description}
+      </div>
 
-  //     -------------------------------------
-  //         <div class="number_rooms">{{ place.number_rooms }} Bedroom{% if place.number_rooms != 1 %}s{% endif %}</div>
-  //           <div class="number_bathrooms">{{ place.number_bathrooms }} Bathroom{% if place.number_bathrooms != 1 %}s{% endif %}</div>
-	//   </div>
+      -------------------------------------
+          <div class="number_rooms">{{ place.number_rooms }} Bedroom{% if place.number_rooms != 1 %}s{% endif %}</div>
+            <div class="number_bathrooms">{{ place.number_bathrooms }} Bathroom{% if place.number_bathrooms != 1 %}s{% endif %}</div>
+	  </div>
 
-  //       */
+        */
         
-  //     // });
-  //   },
-  //   error: function () {
-  //     console.log('We are sorry but our servers are having an issue right now');
-  //   }
-  // })
-
+      });
+    },
+    error: function () {
+      console.log('We are sorry but our servers are having an issue right now');
+    }
+  })
 });
 
 function appendText(field, elementInfo)
@@ -91,43 +98,4 @@ function appendText(field, elementInfo)
     orgin +=  "</div>";
   
   return origin;
-}
-
-async function AjaxCall()
-{
-  let place = null;
-  let user = null;
-  await doAjax("http://afa6415d533b.0a98cdc3.hbtn-cod.io:5001/api/v1/places_search/")
-      .then(data => place = data);
-  await doAjax(`http://afa6415d533b.0a98cdc3.hbtn-cod.io:5001/api/v1/users/${place[0].user_id}`)
-      .then(data => {console.log(data.json());user = data});
-
-    console.log(place);
-    //console.log(user);
-}
-
-async function doAjax(ajaxurl, data = {}) {
-  let response;
-
-  try {
-    response = await $.ajax({
-        url: ajaxurl,
-        type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify(data),
-        xhrFields: {
-          withCredentials: false
-        },  
-        headers: {
-        }
-        // sucess: function (data) {
-        //   console.log("ewewewewewewewewewew");
-        //   response = data
-        // },
-    });
-
-    return response;
-  } catch (error) {
-      console.error(error);
-  }
 }
