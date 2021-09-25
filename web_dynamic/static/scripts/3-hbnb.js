77c0719c817b00cd4835a503edc5ddad3add0054
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-25 12:28:30 -0300

    95%

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -33,17 +33,37 @@ $(document).ready(function () {
       $("div#api_status").removeClass("available");
       return resp.text().then(text => {throw new Error(text)}) 
     });
-    
-    // $.post("http://afa6415d533b.0a98cdc3.hbtn-cod.io:5001/api/v1/places_search/", {},
-    // function(data, status){
-    //   if (status.)
-    //   alert("Data: " + data + "\nStatus: " + status);
-    //   console.log(data);
-    // });
-  
+
+  placeData = getResponse('http://afa6415d533b.0a98cdc3.hbtn-cod.io:5001/api/v1/places_search/');
+  userData = getResponse('http://afa6415d533b.0a98cdc3.hbtn-cod.io:5001/api/v1/users')
+
+  let ownerData = {};
+  placeData.forEach(place => {
+    let str = `<article><div class="title_box"><h2>${place.name}</h2><div class="price_by_night">$${palceData.price_by_night}</div></div>`;
+    str += '<div class="information">';
+    str += appendText("Guest", palceData.max_guest, "max_guest");
+    str += appendText("Bedroom", palceData.number_rooms, "number_rooms");
+    str += appendText("Bathroom", palceData.number_bathrooms, "number_bathrooms");
+    str += "</div>";
+
+    userData.forEach(usr => {
+      if (usr.id == place.user_id)
+      ownerData[place.user_id] = usr;
+    });
+
+    str += `<div class="user"><b>Owner:</b> ${ownerData[place.user_id].first_name} ${ownerData[place.user_id].last_name}</div>`;
+    str += `<div class="description">${place.description}</div>`;
+  });
+  console.log(ownerData);
+
+  $("section.places").append(str);
+});
+
+function getResponse(url) {
+  let resp = null;
   $.ajax({
     type: 'post',
-    url: 'http://afa6415d533b.0a98cdc3.hbtn-cod.io:5001/api/v1/places_search/',   
+    url: url,   
     contentType: 'application/json',
     data: JSON.stringify({}),
     xhrFields: {
@@ -55,40 +75,12 @@ $(document).ready(function () {
     success: function (data) {
       console.log('Success');
       console.log(data);
-      // Loop section
-      data.forEach(element => {
-        console.log(element);
-        let str = `<article><div class="title_box"><h2>${element.name}</h2><div class="price_by_night">$${element.price_by_night}</div></div>`;
-        str += '<div class="information">';
-          str += appendText("Guest", element.max_guest, "max_guest");
-          str += appendText("Bedroom", element.number_rooms, "number_rooms");
-          str += appendText("Bathroom", element.number_bathrooms, "number_bathrooms");
-        str += "</div>";
-        $("section.places").append(str);
-
-        
-        /*
-        <div class="user">
-        <b>Owner:</b> ${element.user.first_name} ${ element.user.last_name}
-      </div>
-      <div class="description">
-      ${element.description}
-      </div>
-
-      -------------------------------------
-          <div class="number_rooms">{{ place.number_rooms }} Bedroom{% if place.number_rooms != 1 %}s{% endif %}</div>
-            <div class="number_bathrooms">{{ place.number_bathrooms }} Bathroom{% if place.number_bathrooms != 1 %}s{% endif %}</div>
-	  </div>
-
-        */
-        
-      });
-    },
-    error: function () {
-      console.log('We are sorry but our servers are having an issue right now');
+      resp = data;
     }
-  })
-});
+  });
+  return (resp);
+}
+
 
 function appendText(field, elementInfo, divClassName)
 {
