009edaaaab08c53a38dd7c2831bb97e5d2c949c5
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-25 11:33:15 -0300

    ee

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
     //AjaxCall();
     doAjax("http://afa6415d533b.0a98cdc3.hbtn-cod.io:5001/api/v1/places_search/")
       .then(data => place = data);
-    doAjax(`http://afa6415d533b.0a98cdc3.hbtn-cod.io:5001/api/v1/users/${place.user_id}`)
+    doAjax(`http://afa6415d533b.0a98cdc3.hbtn-cod.io:5001/api/v1/users/${place[0].user_id}`)
       .then(data => user = data);
 
     console.log(place);
