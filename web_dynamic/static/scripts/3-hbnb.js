c3d75a415dcd8938c3d0ed9e9e405b5b16f97782
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-25 11:41:23 -0300

    a

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -100,10 +100,10 @@ async function AjaxCall()
   await doAjax("http://afa6415d533b.0a98cdc3.hbtn-cod.io:5001/api/v1/places_search/")
       .then(data => place = data);
   await fetch(`http://afa6415d533b.0a98cdc3.hbtn-cod.io:5001/api/v1/users/${place[0].user_id}`)
-      .then(data => user = data);
+      .then(data => {console.log(data);user = data});
 
     console.log(place);
-    console.log(user);
+    //console.log(user);
 }
 
 async function doAjax(ajaxurl, data = {}) {
